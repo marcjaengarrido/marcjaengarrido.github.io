578dd540ebd6d79f0220c9fa0adaee83011e73ae
marcjaengarrido <102189955+marcjaengarrido@users.noreply.github.com>
2024-10-24 22:11:07 +0000

    command system tiny tweak

---
+++ commandOptions.js
@@ -0,0 +1,22 @@
+let commandOptions = [
+    {
+        name: "help",
+        response: "Ayudame waaa waaa"
+    },
+    {
+        name: "alex",
+        response: "Alex msg"
+    },
+    {
+        name: "marc",
+        response: "marc msg"
+    },
+    {
+        name: "marcjaengarrido",
+        response: "marcjaengarrido msg"
+    },
+    {
+        name: "polla",
+        response: "polla msg"
+    }
+]
--- script.js
+++ script.js
@@ -9,18 +9,19 @@ inputTerminal.addEventListener("keydown", function(event) {
 })
 
 inputTerminal.addEventListener("keyup", function(event) {
-    let commandsAvailable = ["help", "polla", "alex", "marc", "marcjaengarrido"];
-    let selectedPrediction = commandsAvailable.find(x=>(inputTerminal.value.length > 0 ? x.startsWith(inputTerminal.value) : false)) ?? (inputTerminal.value.length > 0 ? "" : "help");
+    // let commandsAvailable = ["help", "polla", "alex", "marc", "marcjaengarrido"];
+    let selectedPrediction = commandOptions.find(x=>(inputTerminal.value.length > 0 ? (x.name).startsWith(inputTerminal.value) : false)) ?? (inputTerminal.value.length > 0 ? "" : commandOptions[0]);
 
     if (event.key === "Enter") {
         processCommand(inputTerminal.value)
         inputTerminal.value = "";
+        selectedPrediction = commandOptions[0];
     } else if (event.key === "Tab") {
-        inputTerminal.value = selectedPrediction;
+        inputTerminal.value = selectedPrediction.name;
     }
     
     inputTerminalProcessed.textContent = inputTerminal.value
-    suggestionTerminal.textContent = selectedPrediction.slice(inputTerminal.value.length)
+    suggestionTerminal.textContent = (selectedPrediction.name).slice(inputTerminal.value.length)
 });
 
 
