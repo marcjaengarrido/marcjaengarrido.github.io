11a7c7aa5ed1084e43c93204cf2236a8c5bd9a43
marcjaengarrido <marcjaen15@iernestlluch.cat>
2024-10-24 17:28:40 +0200

    working autocomplete

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const inputTerminal = document.getElementById("inputCMD");
 const inputTerminalProcessed = document.getElementById("currentCMD");
+const suggestionTerminal = document.getElementById("suggestionTerminal");
+
+inputTerminal.addEventListener("keydown", function(event) {
+    if (event.key === "Tab") {
+        event.preventDefault();
+    }
+})
 
 inputTerminal.addEventListener("keyup", function(event) {
+    let commandsAvailable = ["help", "polla", "alex", "marc", "marcjaengarrido"];
+    let selectedPrediction = commandsAvailable.find(x=>(inputTerminal.value.length > 0 ? x.startsWith(inputTerminal.value) : false)) ?? (inputTerminal.value.length > 0 ? "" : "help");
+
     if (event.key === "Enter") {
         processCommand(inputTerminal.value)
         inputTerminal.value = "";
+    } else if (event.key === "Tab") {
+        inputTerminal.value = selectedPrediction;
     }
+    
+    inputTerminalProcessed.textContent = inputTerminal.value
+    suggestionTerminal.textContent = selectedPrediction.slice(inputTerminal.value.length)
 });
+
+
 
 
 function processCommand(command) {
